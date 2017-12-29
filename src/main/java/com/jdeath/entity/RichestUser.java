e2bc733d435500e0f4d50d96e82fee772b8eaa3b
ksereda <alternativer2005@yandex.ru>
2017-12-29 13:07:59 +0300

    add hibernate properties

---
+++ src/main/java/com/jdeath/entity/RichestUser.java
@@ -0,0 +1,47 @@
+package com.jdeath.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class RichestUser {
+    @Id
+    @Column(name = "userId")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "sureName")
+    private String sureName;
+
+    public RichestUser() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSureName() {
+        return sureName;
+    }
+
+    public void setSureName(String sureName) {
+        this.sureName = sureName;
+    }
+}
